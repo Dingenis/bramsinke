23a88b6d7e3f51b9751521c8ac69ea27514f0deb
Bram Sinke <dingenis.sieger@gmail.com>
2022-05-26 10:59:56 +0200

    First Commit

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return <Container>{children}</Container>
+}
+export default Layout
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: []
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  }
+})
+module.exports = withMDX({
+  webpack (config, options) {
+    return config
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+})
